9c2ba23afd62a6f75e0ad59cce16b1b1fa946fa8
bmr <traang180204@gmail.com>
2025-05-13 03:25:41 +0700

    update dtb, home, model, product detail

--- lib/models/brand.dart
+++ lib/models/brand.dart
@@ -1,19 +1,23 @@
 class Brand {
   final String? id;
   final String name;
+  final String imgUrl;
 
   Brand({
     this.id,
     required this.name,
+    required this.imgUrl,
   });
 
   factory Brand.fromJson(Map<String, dynamic> json) => Brand(
         id: json['_id'] as String?,
         name: json['name'] as String,
+        imgUrl: json['img_url'] as String,
       );
 
   Map<String, dynamic> toJson() => {
         if (id != null) '_id': id,
         'name': name,
+        'img': imgUrl
       };
 } 
