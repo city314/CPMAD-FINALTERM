3547e07e6f197c3d93bee45853ac07d5fbc1b0bd
city314 <126061643+city314@users.noreply.github.com>
2025-05-16 12:24:11 +0700

    updating cart route

--- service/CartService/server.js
+++ service/CartService/server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const connectDB = require('./dtb');
 const mongoose = require('mongoose');
@@ -6,6 +7,7 @@ const cors = require('cors');
 
 const tableRoutes = require('./routes/table');
 const userRoutes = require('./routes/user');
+const cartRoutes = require('./routes/cart'); // thêm dòng này
 
 const app = express();
 const PORT = 3003;
@@ -20,8 +22,9 @@ connectDB();
 // Routes
 app.use('/api/table', tableRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/cart', cartRoutes);    // mount route của CartService
 
 // Khởi chạy server
 app.listen(PORT, () => {
-    console.log(`🚀 OrderingService chạy trên cổng ${PORT}`);
+  console.log(`🚀 OrderingService chạy trên cổng ${PORT}`);
 });
